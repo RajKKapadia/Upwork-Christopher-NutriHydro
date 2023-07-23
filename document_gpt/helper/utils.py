3abd2658661ea7719a4f373f9066263467843d34
RajKKapadia <raajforyou@gmail.com>
2023-07-23 21:56:02 +0530

    changed chat history functions

--- document_gpt/helper/utils.py
+++ document_gpt/helper/utils.py
@@ -2,24 +2,35 @@ from pyairtable import Table
 
 from config import config
 
-def get_context(data: list) -> str:
-    context = ''
-    for i in range(0, len(data), 2):
-        if len(data[:-2]) == i:
-            context += f'AI - {data[i]}'
-        else:
-            context += f'AI - {data[i]}\n'
-            context += f'User - {data[i+1]}\n'
-    return context
 
-def get_context_from_mongodb(data: list) -> str:
-    context = ''
+def get_chat_history(data: list) -> str:
+    chat_history = []
+    if len(data) == 2:
+        return chat_history
+    for i in range(1, len(data)-1, 2):
+        chat_history.append(
+            (
+                data[i],
+                data[i+1]
+            )
+        )
+    return chat_history
+
+
+def get_chat_history_from_mongodb(data: list) -> str:
+    chat_history = []
     for d in data:
-        context += f'User: {d["query"]}\n'
-        context += f'AI: {d["response"]}\n'
-    return context
+        chat_history.append(
+            (
+                d['query'],
+                d['response']
+            )
+        )
+    return chat_history
+
 
 table = Table(config.AIRTABLE_API_KEY, config.AIRTABLE_APP_ID, 'users')
+
 
 def create_airtable_user(user: dict) -> None:
     table.create(user)
