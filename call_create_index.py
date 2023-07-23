6e93caac0458e1198d76e271aa7d9373159b4f8f
RajKKapadia <raajforyou@gmail.com>
2023-07-23 21:55:32 +0530

    removed, unwanted

--- call_create_index.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-
-from document_gpt.helper.index import create_index
-
-loop = asyncio.get_event_loop()
-
-loop.run_until_complete(create_index('data/input/commercial_hydroponics.pdf'))
